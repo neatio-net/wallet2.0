832f5eeaa8c847d37b346bec428ef0ea6396fee7
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-09-04 21:12:39 +0300

    Update SendPK.vue

--- src/views/SendPK.vue
+++ src/views/SendPK.vue
@@ -7,9 +7,7 @@
       </div>
     </div>
     <div class="box1">
-      <div class="hero__title-page" v-show="address == null">
-        {{ $t("pages.send.pageName") }}
-      </div>
+      <div class="hero__wallet-description">{{ $t("pages.home.pageName") }}</div>
 
       <div class="hero__title" v-show="address == null">
         <input
