09c87deb27625e710d85b02ba3a84d0664868011
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-09-11 21:25:45 +0300

    Update CreateKS.vue

--- src/views/CreateKS.vue
+++ src/views/CreateKS.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="body-container">
     <div class="hero__title-page">{{ $t("pages.create.pageName") }}</div>
-    <!-- <div class="hero__title">{{ $t("TEXT2") }}</div> -->
     <div class="box1">
     <div class="hero__subTitle2">Please make sure you are using</div>
     <div class="hero__subTitle3">
