1b0cd746ae8eab366a75b15aae8b6a548c642cb4
MrTizu <99123684+tizul@users.noreply.github.com>
2023-11-02 22:22:03 +0200

    Update SendKS.vue

--- src/views/SendKS.vue
+++ src/views/SendKS.vue
@@ -64,7 +64,7 @@
     </div>
     <div class="hero__subTitle2" v-show="balance !== null">Your Balance</div>
     <div class="hero__bal" v-show="balance !== null">
-      {{ balance }} <span class="hero__neat">NEAT</span>
+      {{ balance }} <span class="hero__neat">NIO</span>
     </div>
     <div class="hero__title" v-show="address == null"></div>
     <div class="hero__title" v-show="address !== null">
