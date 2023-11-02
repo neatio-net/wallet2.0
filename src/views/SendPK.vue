3aad0c56dacc9967fb3eb7667ce0743c313b31b1
MrTizu <99123684+tizul@users.noreply.github.com>
2023-11-02 22:22:17 +0200

    Update SendPK.vue

--- src/views/SendPK.vue
+++ src/views/SendPK.vue
@@ -6,7 +6,26 @@
         <b-icon-lock-fill></b-icon-lock-fill> https://wallet.neatio.net
       </div>
     </div>
-    <div class="box1">
+
+    <div class="hero__subTitle2" v-show="address !== null">
+          Address
+        </div>
+        <div class="hero__title">
+          {{ address }}
+        </div>
+
+        <div class="box-btn">
+          <div class="box8" v-show="address !== null">
+          <button class="ripplex" id="rpp1" @click="swStake">Send</button>
+        </div>
+        <div class="box8" v-show="address !== null">
+          <button class="ripplex" id="rpp2" @click="swStake">Stake</button>
+        </div>
+        </div>
+  
+
+
+    <div class="box1" id="box11" >
       <div class="hero__wallet-description" v-show="address == null">
         {{ $t("Here you can access your Neatio wallet.") }}
       </div>
@@ -28,12 +47,12 @@
       </div>
 
       <div class="hero__walletInfo" v-show="address !== null">
-        <div class="hero__subTitle2" v-show="address !== null">
+        <!-- <div class="hero__subTitle2" v-show="address !== null">
           WALLET ADDRESS
         </div>
         <div class="hero__title">
           {{ address }}
-        </div>
+        </div> -->
         <div class="hero__title">
           <div v-show="balance == null && address !== null">
             <div class="breeding-rhombus-spinner">
@@ -50,10 +69,10 @@
           </div>
         </div>
         <div class="hero__subTitle2" v-show="balance !== null">
-          WALLET BALANCE
+          BALANCE
         </div>
         <div class="hero__bal" v-show="balance !== null">
-          {{ balance }} <span class="hero__neat">NEAT</span>
+          {{ balance }} <span class="hero__neat">NIO</span>
         </div>
       </div>
 
@@ -81,7 +100,92 @@
       </div>
 
       <div class="hero__title" v-show="txHash !== null">
-        Executed!
+        Sent!
+        <div class="hero__subTitle2" v-show="txHash !== null">
+          Transaction Hash ID is:
+        </div>
+        <div class="hero__subTitle3" v-show="txHash !== null">
+          <a :href="`https://scan.neatio.net/tx/${txHash}`">View TX</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="box2" id="box22" v-show="address == 1">
+      <div class="hero__wallet-description" v-show="address == null">
+        {{ $t("Here you can access your Neatio wallet.") }}
+      </div>
+
+      <div class="hero__title" v-show="address == null">
+        <input
+          type="password"
+          class="hero__inputs"
+          v-model="keyInput"
+          placeholder=" Enter Your Private Key"
+        />
+      </div>
+
+      <div class="hero__warn" v-show="address == null">{{ keyError }}</div>
+      <div class="hero__title" v-show="address == null">
+        <div class="hero__title-ks">
+          <button class="ripple" @click="importKey">IMPORT</button>
+        </div>
+      </div>
+
+      <div class="hero__walletInfo" v-show="address !== null">
+        <!-- <div class="hero__subTitle2" v-show="address !== null">
+          WALLET ADDRESS
+        </div>
+        <div class="hero__title">
+          {{ address }}
+        </div> -->
+        <div class="hero__title">
+          <div v-show="balance == null && address !== null">
+            <div class="breeding-rhombus-spinner">
+              <div class="rhombus child-1"></div>
+              <div class="rhombus child-2"></div>
+              <div class="rhombus child-3"></div>
+              <div class="rhombus child-4"></div>
+              <div class="rhombus child-5"></div>
+              <div class="rhombus child-6"></div>
+              <div class="rhombus child-7"></div>
+              <div class="rhombus child-8"></div>
+              <div class="rhombus big"></div>
+            </div>
+          </div>
+        </div>
+        <div class="hero__subTitle2" v-show="balance !== null">
+          BALANCE
+        </div>
+        <div class="hero__bal" v-show="balance !== null">
+          {{ balance }} <span class="hero__neat">NIO</span>
+        </div>
+      </div>
+
+      <div class="hero__title" v-show="address !== null"></div>
+      <div class="hero__title" v-show="address !== null">
+        <input
+          type="text"
+          class="hero__input1"
+          v-model="addressToSend"
+          placeholder="Address"
+        />
+      </div>
+      <div class="hero__title" v-show="address !== null">
+        <input
+          type="text"
+          class="hero__input2"
+          v-model="amountToSend"
+          placeholder="Amount"
+        />
+      </div>
+      <div class="hero__title" v-show="address !== null">
+        <div class="hero__title-ks">
+          <button class="ripple" @click="neatSend">Stake</button>
+        </div>
+      </div>
+
+      <div class="hero__title" v-show="txHash !== null">
+        Sent!
         <div class="hero__subTitle2" v-show="txHash !== null">
           Transaction Hash ID is:
         </div>
@@ -94,12 +198,13 @@
 </template>
 
 <script>
+
 const Account = require("nio-api").account;
+const Transaction = require("nio-api").transaction;
+const RPC = require("nio-api").rpc;
 const Nat = require("nio-api").nat;
 const Utils = require("nio-api").utils;
 const URL = "https://rpc.neatio.net";
-const Web3 = require("web3");
-const web3 = new Web3("https://rpc.neatio.net");
 
 import axios from "axios";
 
@@ -118,6 +223,7 @@ export default {
       addressToSend: null,
       keyError: null,
       txHash: null,
+
     };
   },
 
@@ -147,45 +253,61 @@ export default {
             .post(URL, DATA, { "Content-type": "application/json" })
             .then(
               (response) =>
-                (this.balance = Utils.toNEAT(
+                (this.balance = Utils.toNio(
                   Nat.toString(response.data.result)
                 ))
             );
-        }, 500);
+        }, 3500);
       }
     },
 
+
+
     async neatSend() {
-      const privateKey = "0x" + this.keyInput;
-      const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      const addressFrom = account.address;
-      const amountToSend = this.amountToSend;
-      const addressTo = this.addressToSend;
-      console.log(`Sending... ${addressFrom} to ${addressTo}`);
-
-      const createTransaction = await web3.eth.accounts.signTransaction(
-        {
-          from: addressFrom,
-          to: addressTo,
-          value: web3.utils.toWei(`${amountToSend}`, "ether"),
-          gas: "53000",
-        },
-        privateKey
-      );
-
-      const sending = "Sending...";
-      console.log(sending);
-
-      const txHash = await web3.eth.sendSignedTransaction(
-        createTransaction.rawTransaction
-      );
-
-      const transactionHash = txHash.transactionHash;
-      this.txHash = transactionHash;
-
-      console.log(`Transaction confirmed: ${txHash.transactionHash}`);
-      this.txHash = txHash.transactionHash;
+      const userAccount = {
+        address: this.address,
+        privateKey: this.privateKey,
+      };
+      const send = RPC(URL);
+      const nonce = await send("neat_getTransactionCount", [
+        userAccount.address,
+        "latest",
+      ]);
+      const recipient = this.addressToSend;
+      const amount = this.amountToSend;
+      const tx = {
+        chainId: Nat.fromString("1"),
+        nonce: Nat.fromString(nonce),
+        gasPrice: Nat.fromString("10000000000"),
+        gas: Nat.fromString("1000000"),
+        to: Utils.stringToHex(recipient),
+        value: Nat.fromString(Utils.fromNio(amount)),
+        data: "0x",
+      };
+      const signature = Transaction.sign(tx, userAccount);
+      const txHash = await send("neat_sendRawTransaction", [signature]);
+      this.txHash = txHash;
     },
+
+    swStake()  {
+  
+
+            if (document.getElementById('box11')) {
+
+                if (document.getElementById('box11').style.display == 'none') {
+                    document.getElementById('box11').style.display = 'block';
+                    document.getElementById('box22').style.display = 'none';
+                }
+                else {
+                    document.getElementById('box11').style.display = 'none';
+                    document.getElementById('box22').style.display = 'block';
+                }
+                
+            }
+            
+
+}
+
   },
 };
 </script>
