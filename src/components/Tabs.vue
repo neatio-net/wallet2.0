41d62c49ca69f050fe2212db5c94628c51a58ffa
MrTizu <99123684+tizul@users.noreply.github.com>
2023-11-03 23:14:15 +0200

    Create Tabs.vue

---
+++ src/components/Tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <article>
+    <header class="tabs">
+      <ul>
+        <li v-for="(tab, index) in tabs" :key="index">
+           <div class="nav-tab"
+               :class="{ 'is-active' : tab.isActive }"
+               @click="selectTab(tab)">
+               {{ tab.name }}
+           </div>
+        </li>
+      </ul>
+    </header>
+    <section class="tabs-details">
+   <slot>
+
+   </slot>
+    </section>
+  </article>
+</template>
+
+<script>
+export default {
+   data: () => {
+    return {
+        tabs: []
+    }
+   },
+
+   methods: {
+    selectTab(selectedTab) {
+        this.tabs.forEach(tab => {
+            tab.isActive = tab.name === selectedTab.name;
+        });
+    }
+   },
+   created() {
+        this.tabs = this.$children;
+    }
+   
+}
+</script>
+
+
