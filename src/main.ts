a2c7d41e1ed62fa1d07e3380416f5e7c14b30de6
MrTizu <99123684+tizul@users.noreply.github.com>
2023-11-06 20:10:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import i18n from "@/plugins/i18n";
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 
-
-
-
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
